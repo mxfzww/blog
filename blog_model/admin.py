f32189f2a293930c12c77efcf1d1f2e12cd41129
jake <2426595849@qq.com>
2018-03-08 10:18:39 +0800

    创建表结构,前端模块分离

--- blog_model/admin.py
+++ blog_model/admin.py
@@ -4,3 +4,25 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+
+from blog_model.models import Tag,Article,Category,Comment,Links
+
+@admin.register(Tag)
+class Tag(admin.ModelAdmin):
+    pass
+
+@admin.register(Article)
+class Article(admin.ModelAdmin):
+    pass
+
+@admin.register(Category)
+class Category(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class Conment(admin.ModelAdmin):
+    pass
+
+@admin.register(Links)
+class Links(admin.ModelAdmin):
+    pass
